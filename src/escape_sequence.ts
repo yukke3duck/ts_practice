3a816cc0c90499ac3e76836d91ec40327b229a26
yukke3duck <31387999+yukke3duck@users.noreply.github.com>
2024-08-18 15:46:50 +0900

    add template literal and escape sequence

---
+++ src/escape_sequence.ts
@@ -0,0 +1,3 @@
+console.log("Hello \\world!");
+console.log("Hello \nworld!");
+console.log("Hello \u{796d}world!");
---
+++ src/template_literal.ts
@@ -0,0 +1,9 @@
+const message1: string = `Hello
+world!`;
+console.log(message1);
+
+const str1: string = "Hello";
+const str2: string = "world!";
+console.log(`${str1}, ${str2}`);
+
+console.log(`123 + 456 = ${123 + 456}`);
